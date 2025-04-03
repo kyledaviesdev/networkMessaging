09ede2c7b8bb0fd80507a795ca0a376d577f0842
kyledaviesdev <kylemdavies127@gmail.com>
2025-04-03 11:41:07 -0600

    networkMessaging

---
+++ messaging.py
@@ -0,0 +1,48 @@
+import threading
+import socket
+
+host = '127.0.0.1' #localhost
+port = 52525
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host,port))
+server.listen()
+
+users = []
+usernames = []
+
+def anouncement(message):
+    for user in users:
+        user.send(message)
+
+def handle(user):
+    while True:
+        try:
+            message = user.recv(1024)
+            anouncement(message)
+        except:
+            index = users.index(user)
+            users.remove(user)
+            user.close()
+            username = usernames[index]
+            anouncement(f'{username} left...'.encode('ascii'))
+            usernames.remove(username)
+            break
+
+def receive():
+    while True:
+        user, address = server.accept()
+        print(f'Connected with {str(address)}')
+        user.send('NAME'.encode(ascii))
+        username = user.recv(1024).decode('ascii')
+        usernames.append(username)
+        users.append(user)
+
+        print(f'Your username is {username}!')
+        anouncement(f'{username} joined!'.encode('ascii'))
+        user.send("You've connected to the server!".encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(user,))
+        thread.start()
+
+print("Server is live!")
+receive()
---
+++ user.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+
+username = input("Chose a username: ")
+
+user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+user.connect(('127.0.0.1', 52525))
+
+def receive():
+    while True:
+        try:
+            message = user.recv(1024).decode('ascii')
+            if message == 'NAME':
+                user.send(username.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An issue occurred!")
+            user.close()
+            break
+
+def write():
+    while True:
+        message = f'{username}: {input("")}'
+        user.send(message.encode('ascii'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
